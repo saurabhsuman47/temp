9550f624486025944ae3a900741add26cb0ea863
Saurabh Suman <saurabhsuman47@users.noreply.github.com>
2017-11-22 00:42:49 +0530

    Update titanic ml.py

--- titanic ml.py
+++ titanic ml.py
@@ -25,12 +25,17 @@ X = X.fillna(X.mean())
 #X = preprocessing.scale(X)
 y = df.iloc[:, 1]
 
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
 #from sklearn.linear_model import LogisticRegression
 #clf = LogisticRegression()
 #from sklearn.tree import DecisionTreeClassifier
 #clf = DecisionTreeClassifier()
 from sklearn.ensemble import RandomForestClassifier
-clf = RandomForestClassifier(max_depth=4, n_estimators=10)
+#clf = RandomForestClassifier()
+clf = RandomForestClassifier(max_depth=6, n_estimators=10)
 #from sklearn.naive_bayes import GaussianNB
 #clf = GaussianNB()
 #from sklearn.svm import SVC
@@ -39,13 +44,17 @@ clf = RandomForestClassifier(max_depth=4, n_estimators=10)
 #clf = MLPClassifier()
 #from sklearn.ensemble import AdaBoostClassifier
 #clf = AdaBoostClassifier()
+#from sklearn.neighbors import KNeighborsClassifier
+#clf = KNeighborsClassifier(n_neighbors=5, metric='minkowski', p = 2)
 
-clf.fit(X, y)
-y_pred = clf.predict(X)
+clf.fit(X_train, y_train)
+
 
 from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y, y_pred)
-print clf.score(X, y)
+y_pred = clf.predict(X_test)
+cm = confusion_matrix(y_test, y_pred)
+print "train score" , clf.score(X_train, y_train)
+print "test score" , clf.score(X_test, y_test)
 
 #test
 test_filepath =  "/home/saurabh/Downloads/test.csv"
@@ -53,9 +62,8 @@ df1 = pd.read_csv(test_filepath)
 pass_id = df1.iloc[:, 0]
 X_test = df1.iloc[:, [1, 3, 4, 5, 6, 8]]
 labelencoder = preprocessing.LabelEncoder()
-onehotencoder = preprocessing.OneHotEncoder()
 X_test.iloc[:, 1] = labelencoder.fit_transform(X_test.iloc[:, 1])
-X_test = X_test.fillna(X.mean())
+X_test = X_test.fillna(X_test.mean())
 y_test_pred = clf.predict(X_test)
 output = pd.concat([pass_id, pd.DataFrame(y_test_pred, columns=['Survived'])], axis = 1)
 output.to_csv("/home/saurabh/Downloads/foo.csv", index=False)
